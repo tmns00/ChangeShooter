d99f1d71835383a5ad241167c43e668a66dbe6c9
ReoMatsumura <reo-hnf-pc@outlook.jp>
2019-06-18 12:00:35 +0900

    Bossに装甲を追加・HP実装

---
+++ Assets/Scripts/Enemy/Boss/Boss.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int BossHP = 100;
+
+    public GameObject shield;
+    public float shieldInterval = 5;
+    private bool isShield;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isShield = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BossHP <= 0)
+            Dead();
+
+        Debug.Log(isShield);
+
+        if (!isShield)
+        {
+            StartCoroutine(Guard());
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerBullet")
+        {
+            Destroy(other.gameObject);
+
+            BossHP -= 1;
+        }
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator Guard()
+    {
+        isShield = true;
+        yield return new WaitForSeconds(shieldInterval);
+        Instantiate(shield, transform.position, Quaternion.identity);
+    }
+
+    public void ShieldFlag(bool flag)
+    {
+        isShield = flag;
+    }
+}
---
+++ Assets/Scripts/Enemy/Boss/BossShield.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShield : MonoBehaviour
+{
+    private Boss boss;
+
+    private void Awake()
+    {
+        boss = GameObject.Find("Boss").GetComponent<Boss>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            boss.ShieldFlag(false);
+            Destroy(gameObject);
+        }
+
+        if(other.gameObject.tag=="PlayerBullet")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
