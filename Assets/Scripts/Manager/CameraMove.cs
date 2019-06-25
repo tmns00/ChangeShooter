0b54c5c72544e08a4141f112dd250611588ab668
yuuki_kitamoto <y.o.u.k.i03ww@gmail.com>
2019-06-25 16:11:08 +0900

    kameraMove

---
+++ Assets/Scripts/Manager/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    
+    bool isMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMove)
+        {
+            Camera camera = Camera.main;
+            camera.gameObject.transform.Translate(new Vector3(0.05f, 0.0f, 0.0f));
+        }
+    }
+}
